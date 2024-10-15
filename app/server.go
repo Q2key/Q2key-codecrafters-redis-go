ea09252e28063806c62b9764fb4af5de14e59b0a
dmtocque <dmitry.totskiy@fastdev.se>
2024-10-15 23:00:34 +0400

    PONG

--- app/server.go
+++ app/server.go
@@ -16,15 +16,30 @@ func main() {
 
 	//Uncomment this block to pass the first stage
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	ln, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := ln.Accept()
+		go func() {
+			defer conn.Close()
+
+			buff := make([]byte, 1024)
+			_, err = conn.Read(buff)
+
+			switch string(buff)[0:4] {
+			case "PING":
+				_, _ = conn.Write([]byte("+PONG\r\n"))
+			}
+
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err.Error())
+				os.Exit(1)
+			}
+		}()
+
 	}
 }
