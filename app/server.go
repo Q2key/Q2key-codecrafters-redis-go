2cdd672630ca3171504cb0d7e51657ca9a21de82
dmtocque <dmitry.totskiy@fastdev.se>
2024-10-16 01:00:12 +0400

    fix

--- app/server.go
+++ app/server.go
@@ -30,7 +30,7 @@ func main() {
 			buff := make([]byte, 1024)
 
 			_, err = conn.Read(buff)
-			_, _ = conn.Write([]byte("+PONG\r\nPONG\r\n"))
+			_, _ = conn.Write([]byte("+PONG\r\n"))
 
 			if err != nil {
 				fmt.Println("Error accepting connection: ", err.Error())
