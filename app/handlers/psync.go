f46bd0ab54d6c4fb5bef6fe18fa14aa7dafeba42
Dmitrii Totskii <dmitrii@bubblemarble.pro>
2024-12-18 01:01:17 +0400

    codecrafters submit [skip ci]

--- app/handlers/psync.go
+++ app/handlers/psync.go
@@ -22,7 +22,7 @@ func (h *PsyncHandler) Handle(conn contracts.Connector, _ []string, _ *[]byte) {
 
 	h.instance.RegisterReplicaConn(conn)
 
-	mess := fmt.Sprintf("FULLRESYNC %s 0", conn.Id)
+	mess := fmt.Sprintf("FULLRESYNC %s 0", conn.Id())
 	resp := core.FromStringToRedisCommonString(mess)
 
 	rdb := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
